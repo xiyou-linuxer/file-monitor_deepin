7813aa28f6630dae74e28634383be841825e0731
LhdDream <626487728@qq.com>
2019-05-29 23:07:13 +0800

    完善

--- src/start.sh
+++ src/start.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-cd_dir_1="../../etc/";
+cd_dir_1="../etc/";
 cd_dir_2="/etc/";
 name="file_watch";
 cd_dir_3="/etc/file_watch/"
-cd_dir_4="src/client"
+cd_dir_4="../src/client"
+cd_dir_5="../hook/hook.so"
+cd_dir_7="/etc/file_watch/init.conf"
 eval    "cd $cd_dir_2"
 eval    "sudo mkdir $name"
+eval    "sudo chmod 755 $name"
 eval    "cd -"
 eval    "cd $cd_dir_1"
-eval    "cp init.conf $cd_dir_3"
+eval    "sudo cp init.conf $cd_dir_3"
+eval    "sudo chmod 755 $cd_dir_7"
 eval    "cd $cd_dir_4"
-eval    "export $pwd/hook/hook.so"
+eval    "export LD_PRELOAD=$cd_dir_5"
 eval    "make && ./inotify"
+eval    "unset LD_PRELOAD"
