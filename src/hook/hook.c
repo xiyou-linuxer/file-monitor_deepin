b5093a05882b60b0f13d963759e3989a6a22ed53
LhdDream <626487728@qq.com>
2019-05-31 12:14:17 +0800

    完善hook

--- src/hook/hook.c
+++ src/hook/hook.c
@@ -211,10 +211,10 @@ int recv_keep_valie()
 
 int open(const char *pathname, int flags, ...)
 {
-    // if (recv_keep_valie() == 0) {
-	// perror("error your no permission\n");
-	// return -1;
-    // }
+    if (recv_keep_valie() == 0) {
+	perror("error your no permission\n");
+	return -1;
+    }
     /* Some evil injected code goes here. */
     int res = 0;
     char resolved_path[128];
@@ -279,10 +279,10 @@ int open(const char *pathname, int flags, ...)
 
 int close(int fd)
 {
-    // if (recv_keep_valie() == 0 || fd == -1) {
-	// perror("error your no permission\n");
-	// return -1;
-    // }
+    if (recv_keep_valie() == 0 || fd == -1) {
+	perror("error your no permission\n");
+	return -1;
+    }
     char temp_buf[1024] = { '\0' };
     char file_path[1024] = { '0' };	// PATH_MAX in limits.h
     snprintf(temp_buf, sizeof(temp_buf), "/proc/self/fd/%d", fd);
